643e9240333dc54fdb9130b197cf4dff8b6e17ba
ilyamartynov989@gmail.com <ilyamartynov989@gmail.com>
2022-05-07 16:03:01 +0300

    qq

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const backgroundColor = document.querySelector("body");
+const btnStart = document.querySelector("#start");
+const btnStop = document.querySelector("#stop");
+
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function changeBackground(color) {
+  backgroundColor.style.backgroundColor = color;
+}
+
+let intervalColors = 0;
+function funcStart() {
+  btnStart.setAttribute("disabled", "disabled");
+  intervalColors = setInterval(() => {
+    const randomColorFromArray = randomIntegerFromInterval(0, 5);
+    changeBackground(colors[randomColorFromArray]);
+    console.log(randomColorFromArray);
+  }, 1000);
+}
+
+function funcStop() {
+  btnStart.removeAttribute("disabled", "disabled");
+  clearInterval(intervalColors);
+  backgroundColor.style.backgroundColor = "#FFFFFF";
+}
+
+btnStart.addEventListener("click", funcStart);
+btnStop.addEventListener("click", funcStop);
+Я
